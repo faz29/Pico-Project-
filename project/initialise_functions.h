d486417c40e575cfd917f15e61c0cab49b46f1ac
Fares Mohammed <faresmohammed09.02@gmail.com>
2025-04-04 14:04:43 +0100

    controller struct

--- project/initialise_functions.h
+++ project/initialise_functions.h
@@ -18,6 +18,18 @@
 #define M3_pin
 #define M4_pin
 
+#define RAD_TO_DEG 57.295779513082320876798154814105
+#define WHO_AM_I_REG 0x75
+#define PWR_MGMT_1_REG 0x6B
+#define SMPLRT_DIV_REG 0x19
+#define ACCEL_CONFIG_REG 0x1C
+#define ACCEL_XOUT_H_REG 0x3B
+#define TEMP_OUT_H_REG 0x41
+#define GYRO_CONFIG_REG 0x1B
+#define GYRO_XOUT_H_REG 0x43
+#define MPU6050_ADDR 0x68
+
+//struct to hold all the pid variables
 typedef struct {
 //make sure to iniitalise these values that need to be 0 in the main function
     double P;
@@ -37,18 +49,20 @@ typedef struct {
 
 } pid_vars;
 
-//====================================================================================================
-#define RAD_TO_DEG 57.295779513082320876798154814105
-#define WHO_AM_I_REG 0x75
-#define PWR_MGMT_1_REG 0x6B
-#define SMPLRT_DIV_REG 0x19
-#define ACCEL_CONFIG_REG 0x1C
-#define ACCEL_XOUT_H_REG 0x3B
-#define TEMP_OUT_H_REG 0x41
-#define GYRO_CONFIG_REG 0x1B
-#define GYRO_XOUT_H_REG 0x43
-#define MPU6050_ADDR 0x68
+//struct to hold controller data
+typedef struct{
 
+    uint32_t brake;
+    uint32_t throttle;
+    int32_t rx;
+    int32_t ry;
+    int32_t lx;
+    int32_t ly;    
+
+} controller_vars;
+
+
+//====================================================================================================
 // MPU6050 structure
 typedef struct {
 
